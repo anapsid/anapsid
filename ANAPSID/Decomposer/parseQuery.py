be19325f8d414d72dd4a0fe00eb7adfcc3cf631a
Simón Castillo <simoncastillo@gmail.com>
2014-01-08 15:13:14 -0430

    Change of the definition of t_CONSTANT

--- ANAPSID/Decomposer/parseQuery.py
+++ ANAPSID/Decomposer/parseQuery.py
@@ -41,8 +41,8 @@ def t_ID(t):
     t.type = reserved.get(t.value.upper(),'ID')    # Check for reserved words
     return t
 
-t_CONSTANT = r"(\"|\')[^\"\'\n\r]*(\"|\')(@[a-z]|[a-z])?" # According to ISO 639-1, lang tags are specified with two letters.
-#t_CONSTANT = r"(\"|\')[^\"\'\n\r]*(\"|\')(@en)?"
+#t_CONSTANT = r"(\"|\')[^\"\'\n\r]*(\"|\')(@[a-z]|[a-z])?" #According to ISO 639-1, lang tags are specified with two letters.
+t_CONSTANT = r"(\"|\')[^\"\'\n\r]*(\"|\')(@en)?"
 t_VARIABLE = r"([\?]|[\$])([A-Z]|[a-z])\w*"
 t_LKEY = r"\{"
 t_LPAR = r"\("
