dec4aa621605c2815031d0677404353941a2a502
maribelacosta <maribelacosta@Maribels-MacBook-Pro.local>
2014-02-12 00:12:57 +0100

    Minor changes in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='ANAPSID',
-      version='20130619',
-      description='ANAPSID',
-      author='',
-      author_email='anapsid@ldc.usb.ve',
+      version='3.0',
+      description='ANAPSID - An adaptive query processing engine for SPARQL endpoints',
+      author='Maribel Acosta, Maria-Esther Vidal, Gabriela Montoya and Simon Castillo',
+      author_email='mvidal@ldc.usb.ve',
       url='http://www.github.com/anapsid/anapsid',
       scripts=['scripts/run_anapsid', 'scripts/get_predicates'],
       packages=find_packages(exclude=['docs']),
       include_package_data=True,
+      license='GNU/GPL v2'
      )
